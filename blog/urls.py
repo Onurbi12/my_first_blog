4ff0974c0e6e4e9bdba7a2e2aaa4042b064b36f7
bflament <bruno.flament@lapiscine.pro>
2020-06-02 11:45:04 +0200

    First tests with akema mail template test.html

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r"^post/([0-9]+)/$", views.post_detail, name="post_detail"),
+    url(r"^akema/$", views.akema_temp, name="akema_temp"),
     url(r"", views.post_list, name="post_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=int(pk))
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+def akema_temp(request):
+    return render(request, "blog/test.html")
