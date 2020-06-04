ced686e53d93a12ee79ff237380fc44aacfa5625
bflament <bruno.flament@lapiscine.pro>
2020-06-04 17:33:17 +0200

    Encore bokeh ...

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     url(r"^post/([0-9]+)/edit/$", views.post_edit, name="post_edit"),
     url(r"^post/([0-9]+)/$", views.post_detail, name="post_detail"),
-    url(r"^bokeh/$", views.index, name="akema_bokeh"),
     url(r"^akema/$", views.akema_temp, name="akema_temp"),
     url(r"post/new/", views.post_new, name="post_new"),
     url(r"", views.post_list, name="post_list"),
