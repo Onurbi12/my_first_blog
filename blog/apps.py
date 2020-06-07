1dda3c9a152324a132811e5b9f8aed68746c2408
bflament <bruno.flament@lapiscine.pro>
2020-06-07 15:13:11 +0200

    lolilol

--- blog/apps.py
+++ blog/apps.py
@@ -1,17 +1,18 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from bs4 import BeautifulSoup
+
+# from bs4 import BeautifulSoup
 
 
 class BlogConfig(AppConfig):
     name = "blog"
 
 
-with open("./blog/templates/blog/test.html") as html_test:
-    soup = BeautifulSoup(html_test, "html.parser")
+# with open("./blog/templates/blog/test.html") as html_test:
+#     soup = BeautifulSoup(html_test, "html.parser")
 
-print(soup)
-for element in soup.findAll("a"):
-    print(element)
-    print(element.coords)
+# print(soup)
+# for element in soup.findAll("a"):
+#     print(element)
+#     print(element.coords)
